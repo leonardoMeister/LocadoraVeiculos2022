f625b3066cfc8b8bda9a426abea985bed17ce44b
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-21 15:59:31 -0300

    Add project files.

---
+++ LocadoraVeiculos.Repositorio/shared/MapeadorBase.cs
@@ -0,0 +1,23 @@
+﻿using LocadoraVeiculos.Dominio.shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Repositorio.shared
+{
+    public abstract class MapeadorBase<T> where T : EntidadeBase
+    {
+        public abstract Dictionary<string, object> ObtemParametrosRegistro(T registro);
+
+        public abstract T ConverterEmRegistro(IDataReader dataReader);
+
+        public Dictionary<string, object> AdicionarParametro(string campo, object valor)
+        {
+            return new Dictionary<string, object>() { { campo, valor } };
+        }
+    }
+
+}
