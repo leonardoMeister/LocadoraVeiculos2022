237d10284c58bfcc96b405a8df2e0bb75d473456
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-06-23 14:45:51 -0300

    Dominio Taxas

--- LocadoraVeiculos.Dominio/ModuloTaxas/Taxas.cs
+++ LocadoraVeiculos.Dominio/ModuloTaxas/Taxas.cs
@@ -7,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace LocadoraVeiculos.Dominio.ModuloTaxas
 {
-    public class Taxas: EntidadeBase
+    public class Taxas : EntidadeBase
     {
+        public Taxas(string descricao, decimal valor)
+        {
+            Descricao = descricao;
+            Valor = valor;
+        }
 
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
     }
 }
--- LocadoraVeiculos.Dominio/ModuloTaxas/ValidadorTaxas.cs
+++ LocadoraVeiculos.Dominio/ModuloTaxas/ValidadorTaxas.cs
@@ -10,6 +10,16 @@ namespace LocadoraVeiculos.Dominio.ModuloTaxas
 {
     public class ValidadorTaxas: AbstractValidator<Taxas>
     {
+        public ValidadorTaxas()
+        {
+            RuleFor(x => x.Descricao)
+                .MinimumLength(2).WithMessage("A Descrição deve ter no minimo 2 caracteres")
+                .NotNull().WithMessage("Deve ser inserido uma Descrição")
+                .NotEmpty().WithMessage("Deve ser inserido uma Descrição");
 
+            RuleFor(x => x.Valor)
+                .NotNull().WithMessage("Deve ser inserido um valor")
+                .NotEmpty().WithMessage("Deve ser inserido um valor");
+        }
     }
 }
