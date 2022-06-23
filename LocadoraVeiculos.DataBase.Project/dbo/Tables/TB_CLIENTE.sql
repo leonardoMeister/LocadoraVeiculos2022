6168c0e5196949ff821536deab1b0b25bd31dec6
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-23 15:14:08 -0300

    Criacao da tabela cliente

---
+++ LocadoraVeiculos.DataBase.Project/dbo/Tables/TB_CLIENTE.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TB_CLIENTE] (
+    [id_cliente]  INT          IDENTITY (1, 1) NOT NULL,
+    [nome]        VARCHAR (50) NULL,
+    [cpf]         VARCHAR (50) NULL,
+    [endereco]    VARCHAR (50) NULL,
+    [email]       VARCHAR (50) NULL,
+    [telefone]    VARCHAR (50) NULL,
+    [tipoCliente] VARCHAR (50) NULL,
+    [cnh]         VARCHAR (50) NULL
+);
+
