65e22a39e8f6b53f0890fb7ffcbbc21897e390f2
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-06-27 16:45:40 -0300

    Testes Unitarios Veiculos Teste e Taxas teste

---
+++ LocadoraVeiculos.Testes/TestesUnitarios/TesteUnitarioTaxas/UnitTaxasTeste.cs
@@ -0,0 +1,41 @@
+﻿using LocadoraVeiculos.Dominio.ModuloTaxas;
+using LocadoraVeiculos.RepositorioProject.ModuloTaxas;
+using LocadoraVeiculos.RepositorioProject.shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LocadoraVeiculos.Testes.TestesUnitarios.TesteUnitarioTaxas
+{
+    [TestClass]
+    public class UnitTaxasTeste
+    {
+        [TestMethod]
+        public void DeveImpedirCriarTaxaSemValor()
+        {
+            ValidadorTaxas validador = new ValidadorTaxas();
+
+            Taxas taxas = new Taxas("Aluguel Onix", 0);
+            var result = validador.Validate(taxas);
+            Assert.AreEqual(result.IsValid, false);
+        }
+
+        [TestMethod]
+        public void DeveImpedirCriarTaxaDescricao1Caractere()
+        {
+            ValidadorTaxas validador = new ValidadorTaxas();
+
+            Taxas taxas = new Taxas("A", 1800);
+            var result = validador.Validate(taxas);
+            Assert.AreEqual(result.IsValid, false);
+        }
+
+        [TestMethod]
+        public void DevePermitirCriarTaxas()
+        {
+            ValidadorTaxas validador = new ValidadorTaxas();
+            Taxas taxas = new Taxas("Aluguel Onix", 1500);
+            var result = validador.Validate(taxas);
+            Assert.AreEqual(result.IsValid, true);
+
+        }
+    }
+}
