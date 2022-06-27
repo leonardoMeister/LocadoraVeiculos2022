7f1bfb85553147f98ccfff4adfc9ac0b552098fb
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-27 13:01:12 -0300

    #6 configuracao e criacao da estrutura winapp de cliente

--- LocadoraVeiculos.WinApp/ModuloTaxa/TelaAgrupamentoTaxaForm.Designer.cs
+++ LocadoraVeiculos.WinApp/ModuloTaxa/TelaAgrupamentoTaxaForm.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TelaAgrupamentoTaxaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaAgrupamentoTaxaForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TelaAgrupamentoTarefaForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
