aaf4559042a3baede53c5196963f312c82f96a4f
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-06-29 15:26:48 -0300

    Fix Tab index Cadastro Taxa

--- LocadoraVeiculos.WinApp/ModuloTaxa/TelaCadastroTaxaForm.Designer.cs
+++ LocadoraVeiculos.WinApp/ModuloTaxa/TelaCadastroTaxaForm.Designer.cs
@@ -41,52 +41,56 @@
             // laId
             // 
             this.laId.AutoSize = true;
-            this.laId.Location = new System.Drawing.Point(57, 20);
+            this.laId.Location = new System.Drawing.Point(50, 15);
             this.laId.Name = "laId";
-            this.laId.Size = new System.Drawing.Size(58, 20);
+            this.laId.Size = new System.Drawing.Size(46, 15);
             this.laId.TabIndex = 6;
             this.laId.Text = "Id Taxa:";
             // 
             // laNome
             // 
             this.laNome.AutoSize = true;
-            this.laNome.Location = new System.Drawing.Point(12, 74);
+            this.laNome.Location = new System.Drawing.Point(10, 56);
             this.laNome.Name = "laNome";
-            this.laNome.Size = new System.Drawing.Size(110, 20);
+            this.laNome.Size = new System.Drawing.Size(87, 15);
             this.laNome.TabIndex = 7;
             this.laNome.Text = "Descrição Taxa:";
             // 
             // txtDescricao
             // 
-            this.txtDescricao.Location = new System.Drawing.Point(138, 71);
+            this.txtDescricao.Location = new System.Drawing.Point(121, 53);
+            this.txtDescricao.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtDescricao.Name = "txtDescricao";
-            this.txtDescricao.Size = new System.Drawing.Size(273, 27);
-            this.txtDescricao.TabIndex = 8;
+            this.txtDescricao.Size = new System.Drawing.Size(239, 23);
+            this.txtDescricao.TabIndex = 2;
             // 
             // txtId
             // 
             this.txtId.Enabled = false;
-            this.txtId.Location = new System.Drawing.Point(138, 17);
+            this.txtId.Location = new System.Drawing.Point(121, 13);
+            this.txtId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(273, 27);
-            this.txtId.TabIndex = 9;
+            this.txtId.Size = new System.Drawing.Size(239, 23);
+            this.txtId.TabIndex = 1;
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(296, 177);
+            this.btnSalvar.Location = new System.Drawing.Point(259, 133);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(115, 44);
-            this.btnSalvar.TabIndex = 10;
+            this.btnSalvar.Size = new System.Drawing.Size(101, 33);
+            this.btnSalvar.TabIndex = 5;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(170, 177);
+            this.btnCancelar.Location = new System.Drawing.Point(149, 133);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(115, 44);
-            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Size = new System.Drawing.Size(101, 33);
+            this.btnCancelar.TabIndex = 4;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
@@ -94,26 +98,27 @@
             // laValor
             // 
             this.laValor.AutoSize = true;
-            this.laValor.Location = new System.Drawing.Point(36, 130);
+            this.laValor.Location = new System.Drawing.Point(32, 98);
             this.laValor.Name = "laValor";
-            this.laValor.Size = new System.Drawing.Size(79, 20);
+            this.laValor.Size = new System.Drawing.Size(62, 15);
             this.laValor.TabIndex = 12;
             this.laValor.Text = "Valor Taxa:";
             // 
             // txtValor
             // 
-            this.txtValor.Location = new System.Drawing.Point(138, 127);
+            this.txtValor.Location = new System.Drawing.Point(121, 95);
+            this.txtValor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtValor.Mask = "99999999";
             this.txtValor.Name = "txtValor";
-            this.txtValor.Size = new System.Drawing.Size(273, 27);
-            this.txtValor.TabIndex = 13;
+            this.txtValor.Size = new System.Drawing.Size(239, 23);
+            this.txtValor.TabIndex = 3;
             this.txtValor.ValidatingType = typeof(int);
             // 
             // TelaCadastroTaxaForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 248);
+            this.ClientSize = new System.Drawing.Size(374, 186);
             this.Controls.Add(this.txtValor);
             this.Controls.Add(this.laValor);
             this.Controls.Add(this.btnCancelar);
@@ -122,6 +127,7 @@
             this.Controls.Add(this.txtDescricao);
             this.Controls.Add(this.laNome);
             this.Controls.Add(this.laId);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "TelaCadastroTaxaForm";
