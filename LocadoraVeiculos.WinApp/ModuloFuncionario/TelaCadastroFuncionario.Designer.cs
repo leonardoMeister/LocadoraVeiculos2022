fda09e78a4156ffe3c4d7d23138da4fbcf2b3234
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-06-29 00:34:03 -0300

    init a make windown funcionario

--- LocadoraVeiculos.WinApp/ModuloFuncionario/TelaCadastroFuncionario.Designer.cs
+++ LocadoraVeiculos.WinApp/ModuloFuncionario/TelaCadastroFuncionario.Designer.cs
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtSalario = new System.Windows.Forms.MaskedTextBox();
+            this.laValor = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.laNome = new System.Windows.Forms.Label();
+            this.laId = new System.Windows.Forms.Label();
+            this.txtTipoPerfil = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtData = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(131, 128);
+            this.txtSalario.Mask = "99999999";
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(273, 27);
+            this.txtSalario.TabIndex = 21;
+            this.txtSalario.ValidatingType = typeof(int);
+            // 
+            // laValor
+            // 
+            this.laValor.AutoSize = true;
+            this.laValor.Location = new System.Drawing.Point(76, 282);
+            this.laValor.Name = "laValor";
+            this.laValor.Size = new System.Drawing.Size(49, 20);
+            this.laValor.TabIndex = 20;
+            this.laValor.Text = "Login:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(163, 385);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(115, 44);
+            this.btnCancelar.TabIndex = 19;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(289, 385);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(115, 44);
+            this.btnSalvar.TabIndex = 18;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(131, 18);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(273, 27);
+            this.txtId.TabIndex = 17;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(131, 72);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(273, 27);
+            this.txtNome.TabIndex = 16;
+            // 
+            // laNome
+            // 
+            this.laNome.AutoSize = true;
+            this.laNome.Location = new System.Drawing.Point(72, 72);
+            this.laNome.Name = "laNome";
+            this.laNome.Size = new System.Drawing.Size(53, 20);
+            this.laNome.TabIndex = 15;
+            this.laNome.Text = "Nome:";
+            // 
+            // laId
+            // 
+            this.laId.AutoSize = true;
+            this.laId.Location = new System.Drawing.Point(67, 18);
+            this.laId.Name = "laId";
+            this.laId.Size = new System.Drawing.Size(58, 20);
+            this.laId.TabIndex = 14;
+            this.laId.Text = "Id Taxa:";
+            // 
+            // txtTipoPerfil
+            // 
+            this.txtTipoPerfil.Location = new System.Drawing.Point(131, 180);
+            this.txtTipoPerfil.Name = "txtTipoPerfil";
+            this.txtTipoPerfil.Size = new System.Drawing.Size(273, 27);
+            this.txtTipoPerfil.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Senha:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Salario:";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(131, 282);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(273, 27);
+            this.txtLogin.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 20);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Data Admissão:";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(131, 335);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(273, 27);
+            this.txtSenha.TabIndex = 29;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Tipo Perfil:";
+            // 
+            // txtData
+            // 
+            this.txtData.CustomFormat = "";
+            this.txtData.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtData.Location = new System.Drawing.Point(131, 232);
+            this.txtData.MaxDate = new System.DateTime(2030, 1, 1, 0, 0, 0, 0);
+            this.txtData.MinDate = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(273, 27);
+            this.txtData.TabIndex = 30;
             // 
             // TelaCadastroFuncionario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(433, 450);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTipoPerfil);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.laValor);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.laNome);
+            this.Controls.Add(this.laId);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "TelaCadastroFuncionario";
@@ -42,9 +213,27 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TelaCadastroFuncionario";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox txtSalario;
+        private System.Windows.Forms.Label laValor;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label laNome;
+        private System.Windows.Forms.Label laId;
+        private System.Windows.Forms.TextBox txtTipoPerfil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker txtData;
     }
 }
--- LocadoraVeiculos.WinApp/ModuloFuncionario/TelaCadastroFuncionario.cs
+++ LocadoraVeiculos.WinApp/ModuloFuncionario/TelaCadastroFuncionario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LocadoraVeiculos.Dominio.ModuloFuncionario;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,81 @@ namespace LocadoraVeiculos.WinApp.ModuloFuncionario
 {
     public partial class TelaCadastroFuncionario : Form
     {
-        public TelaCadastroFuncionario()
+        private Funcionario funcionario; 
+        TelaPrincipalForm telaPrincipal;
+
+        public Funcionario Funcionario
+        {
+            get { return funcionario; }
+            set
+            {
+                funcionario = value;
+
+                txtId.Text = Convert.ToString(funcionario._id);
+                txtNome.Text = funcionario.Nome;
+                txtSalario.Text = Convert.ToString(funcionario.Salario);
+                txtTipoPerfil.Text = funcionario.TipoPerfil;
+                txtData.Value = funcionario.DataAdmicao;
+                txtLogin.Text = funcionario.Login;
+                txtSenha.Text = funcionario.Senha;
+            }
+        }
+        public TelaCadastroFuncionario(TelaPrincipalForm telaPrincipal)
         {
             InitializeComponent();
+            this.telaPrincipal = telaPrincipal;
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            bool tudoValido = true;
+
+            if (!PegarObjetoTela())
+                tudoValido = false;
+
+            if (ObjetoForInvalido())
+                tudoValido = false;
+
+            if (tudoValido)
+                this.DialogResult = DialogResult.OK;
+        }
+
+        private bool ObjetoForInvalido()
+        {
+            return false;
+            //VALIDAR AQUI
+            //DEPOIS JOGAR A MENSAGEM NO RODAPE
+        }
+
+        private bool PegarObjetoTela()
+        {
+            int id = 0;
+
+            if (txtId.Text != "")
+                id = Convert.ToInt32(txtId.Text);
+
+            if (txtLogin.Text == "" || txtNome.Text == "" || txtSalario.Text == ""
+                || txtSenha.Text == "" || txtTipoPerfil.Text == "" )
+                return false;
+
+            string nome = txtNome.Text;
+            string login = txtLogin.Text;
+            string senha = txtSenha.Text;
+            decimal salario = Convert.ToDecimal(txtSalario.Text);
+            DateTime dataAdmicao = txtData.Value;
+            string tipoPerfil = txtTipoPerfil.Text;
+            
+            funcionario = new Funcionario(nome,login,senha,salario,dataAdmicao,tipoPerfil);
+            funcionario._id = id;
+
+            return true;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            telaPrincipal.AtualizarRodape("Inserção Cancelada.");
+            this.DialogResult = DialogResult.Cancel;
         }
     }
 }
