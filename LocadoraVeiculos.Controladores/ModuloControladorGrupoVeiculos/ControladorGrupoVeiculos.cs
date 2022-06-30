92c6d0166d76e5c3e65d207f3d13cbf142f47b17
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-06-30 15:17:38 -0300

    Validação não possuir Nome repitido Grupo Veiculos

--- LocadoraVeiculos.Controladores/ModuloControladorGrupoVeiculos/ControladorGrupoVeiculos.cs
+++ LocadoraVeiculos.Controladores/ModuloControladorGrupoVeiculos/ControladorGrupoVeiculos.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using FluentValidation.Results;
 using LocadoraVeiculos.Dominio.ModuloGrupoVeiculos;
 using LocadoraVeiculos.Repositorio.shared;
 using LocadoraVeiculos.RepositorioProject.ModuloGrupoVeiculos;
@@ -15,6 +16,40 @@ namespace LocadoraVeiculos.Controladores.ModuloControladorGrupoVeiculos
         protected override AbstractValidator<GrupoVeiculos> PegarValidador()
         {
             return new ValidadorGrupoVeiculos();
+        }
+
+        public override ValidationResult Editar(GrupoVeiculos registro)
+        {
+            var validacaoBanco = FuncionarioForValidoParaEditar(registro);
+
+            if (validacaoBanco.IsValid) return base.Editar(registro);
+            else return validacaoBanco;
+        }
+
+        public override ValidationResult InserirNovo(GrupoVeiculos registro)
+        {
+            var validacaoBanco = FuncionarioForValidoParaInserir(registro);
+            if (validacaoBanco.IsValid) return base.InserirNovo(registro);
+            else return validacaoBanco;
+        }
+        private ValidationResult FuncionarioForValidoParaEditar(GrupoVeiculos registro)
+        {
+            ValidationResult valido = new ValidationResult();
+
+            var func1 = ((RepositorioGrupoVeiculos)Repositorio).SelecionarPorNome(registro.NomeGrupo);
+            if (func1 != null && func1._id != registro._id) valido.Errors.Add(new ValidationFailure("Nome", "Nao pode ter nome repetido"));
+
+            return valido;
+        }
+        private ValidationResult FuncionarioForValidoParaInserir(GrupoVeiculos registro)
+        {
+            ValidationResult valido = new ValidationResult();
+
+            var func1 = ((RepositorioGrupoVeiculos)Repositorio).SelecionarPorNome(registro.NomeGrupo);
+            if (func1 != null) valido.Errors.Add(new ValidationFailure("Nome", "Nao pode ter nome repetido"));
+
+            return valido;
+
         }
     }
 }
