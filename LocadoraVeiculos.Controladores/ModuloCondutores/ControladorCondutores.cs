fc8664a7b28377139faf20c92110ed5f00565232
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-06-30 16:14:03 -0300

    Controladores Base Milestone 2

---
+++ LocadoraVeiculos.Controladores/ModuloCondutores/ControladorCondutores.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using LocadoraVeiculos.Dominio.ModuloCondutores;
+using LocadoraVeiculos.Repositorio.shared;
+using LocadoraVeiculos.RepositorioProject.ModuloCondutores;
+using System;
+
+namespace LocadoraVeiculos.Controladores.ModuloCondutores
+{
+    public class ControladorCondutores : Controlador<Condutores>
+    {
+        protected override IRepository<Condutores> PegarRepositorio()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AbstractValidator<Condutores> PegarValidador()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LocadoraVeiculos.Controladores/ModuloVeiculo/ControladorVeiculo.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using LocadoraVeiculos.Dominio.ModuloVeiculo;
+using LocadoraVeiculos.Repositorio.shared;
+using LocadoraVeiculos.RepositorioProject.ModuloFuncionario;
+using System;
+
+namespace LocadoraVeiculos.Controladores.ModuloVeiculo
+{
+    public class ControladorVeiculo : Controlador<Veiculo>
+    {
+        protected override IRepository<Veiculo> PegarRepositorio()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AbstractValidator<Veiculo> PegarValidador()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
