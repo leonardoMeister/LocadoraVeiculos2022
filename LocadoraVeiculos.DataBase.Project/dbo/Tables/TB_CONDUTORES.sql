42ae1334d9e6a5ae7d07af181e9f1b9933e30113
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-07-01 15:51:34 -0300

    DB Tb Condutores

---
+++ LocadoraVeiculos.DataBase.Project/dbo/Tables/TB_CONDUTORES.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TB_CONDUTORES] (
+    [id_condutores]  INT          IDENTITY (1, 1) NOT NULL,
+    [nome]        VARCHAR (50) NULL,
+    [cpf]         VARCHAR (50) NULL,
+    [endereco]    VARCHAR (50) NULL,
+    [email]       VARCHAR (50) NULL,
+    [telefone]    VARCHAR (50) NULL,
+    [cnh]         VARCHAR (50) NULL,
+    [validadeCnh]        VARCHAR (50) NULL,
+    CONSTRAINT [PK_TB_CONDUTORES] PRIMARY KEY CLUSTERED ([id_condutores] ASC)
+);
