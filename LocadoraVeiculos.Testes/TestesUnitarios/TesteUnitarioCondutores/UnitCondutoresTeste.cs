9b061cc1b6e391c08ad73b2b5843f7bc104847ca
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-07-04 16:40:09 -0300

    Teste Unitarios Condutores

---
+++ LocadoraVeiculos.Testes/TestesUnitarios/TesteUnitarioCondutores/UnitCondutoresTeste.cs
@@ -0,0 +1,41 @@
+﻿using LocadoraVeiculos.Dominio.ModuloCondutores;
+using LocadoraVeiculos.RepositorioProject.ModuloCondutores;
+using LocadoraVeiculos.RepositorioProject.shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LocadoraVeiculos.Testes.TestesUnitarios.TesteUnitarioCondutores
+{
+    [TestClass]
+    public class UnitCondutoresTeste
+    {
+        [TestMethod]
+        public void DeveImpedirCriarCondutoresSemNome()
+        {
+            ValidadorCondutores validador = new ValidadorCondutores();
+
+            Condutores condutores = new Condutores("", "02359919994", "Andre Gargioni", "emailteste@gmail.com", "99-99999-9999", "dasdasdasdasdas", "segunda - feira, 4 de julho de 2022");
+            var result = validador.Validate(condutores);
+            Assert.AreEqual(result.IsValid, false);
+        }
+
+        [TestMethod]
+        public void DeveImpedirCriarCondutoresNome2Caractere()
+        {
+            ValidadorCondutores validador = new ValidadorCondutores();
+
+            Condutores condutores = new Condutores("da", "02359919994", "Andre Gargioni", "emailteste@gmail.com", "99-99999-9999", "dasdasdasdasdas", "segunda - feira, 4 de julho de 2022");
+            var result = validador.Validate(condutores);
+            Assert.AreEqual(result.IsValid, false);
+        }
+
+        [TestMethod]
+        public void DevePermitirCriarCondutores()
+        {
+            ValidadorCondutores validador = new ValidadorCondutores();
+            Condutores condutores = new Condutores("Gustavo Paes", "02359919994", "Andre Gargioni", "emailteste@gmail.com", "99-99999-9999", "dasdasdasdasdas", "segunda - feira, 4 de julho de 2022");
+            var result = validador.Validate(condutores);
+            Assert.AreEqual(result.IsValid, true);
+
+        }
+    }
+}
