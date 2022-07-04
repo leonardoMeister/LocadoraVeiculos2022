0b8bb5ea8f58b4f51b895f18906260b5d495cc68
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-07-04 09:49:08 -0300

    Adicionando tabela plano cobranca

---
+++ LocadoraVeiculos.DataBase.Project/dbo/Tables/TB_PLANOCOBRANCA.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TB_PLANOCOBRANCA] (
+    [id_plano]               INT          IDENTITY (1, 1) NOT NULL,
+    [tipo_plano]             VARCHAR (50) NOT NULL,
+    [valor_por_diario]       DECIMAL (18) NULL,
+    [limite_de_kilometragem] DECIMAL (18) NULL,
+    [valor_por_km]           DECIMAL (18) NULL,
+    [grupo_veiculo_id]       INT          NOT NULL,
+    CONSTRAINT [PK_TB_PLANOCOBRANCA] PRIMARY KEY CLUSTERED ([id_plano] ASC),
+    CONSTRAINT [FK_grupoVeiculo_PlanoCobranca] FOREIGN KEY ([grupo_veiculo_id]) REFERENCES [dbo].[TB_GRUPOVEICULOS] ([id_grupoveiculos])
+);
+
