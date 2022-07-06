83a1e03765cc0652b48a98e18519a1bf66a04f16
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-07-06 17:27:36 -0300

    Log Inserir e Editar Cliente Aplicação

--- LocadoraVeiculos.Controladores/ModuloControladorCliente/ControladorCliente.cs
+++ LocadoraVeiculos.Controladores/ModuloControladorCliente/ControladorCliente.cs
@@ -3,11 +3,7 @@ using FluentValidation.Results;
 using LocadoraVeiculos.Dominio.ModuloCliente;
 using LocadoraVeiculos.Repositorio.shared;
 using LocadoraVeiculos.RepositorioProject.ModuloCliente;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Serilog;
 
 namespace LocadoraVeiculos.Controladores.ModuloControladorCliente
 {
@@ -26,18 +22,62 @@ namespace LocadoraVeiculos.Controladores.ModuloControladorCliente
 
         public override ValidationResult Editar(Cliente registro)
         {
-            var validacaoBanco = FuncionarioForValidoParaEditar(registro);
+            Log.Logger.Debug("Tentando editar um Cliente... {@f}", registro);
 
-            if (validacaoBanco.IsValid) return base.Editar(registro);
-            else return validacaoBanco;
+            var validacaoBanco = FuncionarioForValidoParaEditar(registro);
+            if (validacaoBanco.IsValid)
+            {
+                Log.Logger.Debug("Cliente {ClienteNome} editado com sucesso", registro.Nome);
+
+                return base.Editar(registro);
+            }
+            else
+            {
+                foreach (var erros in validacaoBanco.Errors)
+                {
+                    Log.Logger.Warning("Falha ao tentar editar um Cliente {ClienteNome} - {Motivo}",
+                        registro.Nome, erros.ErrorMessage);
+                    return validacaoBanco;
+                }
+            }
+
+            return validacaoBanco;
         }
+
+        //public override ValidationResult InserirNovo(Cliente registro) // BKP ANTES TESTE
+        //{
+        //    var validacaoBanco = FuncionarioForValidoParaInserir(registro);
+        //    if (validacaoBanco.IsValid) 
+        //        return base.InserirNovo(registro);
+        //    else 
+        //        return validacaoBanco;
+        //}
 
         public override ValidationResult InserirNovo(Cliente registro)
         {
+            Log.Logger.Debug("Tentando inserir um Cliente... {@f}", registro);
+
             var validacaoBanco = FuncionarioForValidoParaInserir(registro);
-            if (validacaoBanco.IsValid) return base.InserirNovo(registro);
-            else return validacaoBanco;
+            if (validacaoBanco.IsValid)
+            {
+                Log.Logger.Debug("Cliente {ClienteNome} inserido com sucesso", registro.Nome);
+
+                return base.InserirNovo(registro);
+            }
+            else
+            {
+                foreach (var erros in validacaoBanco.Errors)
+                {
+                    Log.Logger.Warning("Falha ao tentar inserir um Cliente {ClienteNome} - {Motivo}",
+                        registro.Nome, erros.ErrorMessage);
+                    return validacaoBanco;
+                }
+            }
+
+            return validacaoBanco;
         }
+
+
         private ValidationResult FuncionarioForValidoParaEditar(Cliente registro)
         {
             ValidationResult valido = new ValidationResult();
