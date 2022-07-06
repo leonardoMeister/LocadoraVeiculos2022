37fcaaa0ae3a21f31fb4a2b975fc2ccb5daca854
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-07-06 15:58:21 -0300

    Configuracao basica das telas de plano de cobranca

---
+++ LocadoraVeiculos.WinApp/ModuloPlanoCobranca/ConfiguracaoToolBoxPlanoCobranca.cs
@@ -0,0 +1,28 @@
+﻿using LocadoraVeiculos.WinApp.shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.WinApp.ModuloPlanoCobranca
+{
+    public class ConfiguracaoToolBoxPlanoCobranca : ConfiguracaoToolboxBase
+    {
+        public override string TipoCadastro => "Plano de Cobrança";
+
+        public override string TooltipInserir => "";
+
+        public override string TooltipEditar => "";
+
+        public override string TooltipExcluir => "";
+
+        public override string TooltipAdicionarItens => "";
+
+        public override string TooltipAtualizarItens => "";
+
+        public override string TooltipFiltrar => "";
+
+        public override string TooltipAgrupar => "";
+    }
+}
---
+++ LocadoraVeiculos.WinApp/ModuloPlanoCobranca/TelaCadastroPlanoCobranca.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.Results;
+using LocadoraVeiculos.Dominio.ModuloPlanoCobranca;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraVeiculos.WinApp.ModuloPlanoCobranca
+{
+    public partial class TelaCadastroPlanoCobranca : Form
+    {
+        private PlanoCobranca planoCobranca;
+        public Action<string> AtualizarRodape { get; set; }
+        public Func<PlanoCobranca, ValidationResult> GravarRegistro { get; internal set; }
+        public PlanoCobranca PlanoCobranca
+        {
+            get { return planoCobranca; }
+            set
+            {
+                planoCobranca = value;
+
+                //txtId.Text = Convert.ToString(grupoVeiculos._id);
+                //txtNome.Text = grupoVeiculos.NomeGrupo;
+            }
+        }
+
+        public TelaCadastroPlanoCobranca()
+        {
+            InitializeComponent();
+        }
+    }
+}
