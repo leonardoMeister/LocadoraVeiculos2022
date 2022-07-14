392b59ad82969e6f896c0d82d8c3c6e5fefe9d39
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-07-14 16:29:33 -0300

    Integrated Tests GrupoVeiculos

--- LocadoraVeiculos.Testes/TestesIntegradorBanco/TesteIntegradoGrupoVeiculos/IntegratedTestsGrupoVeiculos.cs
+++ LocadoraVeiculos.Testes/TestesIntegradorBanco/TesteIntegradoGrupoVeiculos/IntegratedTestsGrupoVeiculos.cs
@@ -23,29 +23,25 @@ namespace LocadoraVeiculos.Testes.TestesIntegradorBanco.TesteIntegradoGrupoVeicu
         public void DeveInserirGrupoVeiculos()
         {
             ServicoGrupoVeiculos repo = new ServicoGrupoVeiculos();
-
             GrupoVeiculos gveh = new GrupoVeiculos("Grupo 1");
-
             repo.InserirNovo(gveh);
 
-            var gveiculos = repo.SelecionarPorId(gveh._id);
+            var gveiculos = repo.SelecionarPorId(gveh._id).Value;
 
-            Assert.AreEqual(gveiculos._id, gveh._id);
-            Assert.AreEqual(gveiculos.NomeGrupo, gveh.NomeGrupo);
+            Assert.AreEqual(gveiculos, gveh);
         }
 
         [TestMethod]
         public void DeveBuscarVariosGrupoVeiculos()
         {
             ServicoGrupoVeiculos repo = new ServicoGrupoVeiculos();
-
             GrupoVeiculos gveh = new GrupoVeiculos("Grupo 1");
             GrupoVeiculos gveh2 = new GrupoVeiculos("Grupo 2");
 
             repo.InserirNovo(gveh);
             repo.InserirNovo(gveh2);
 
-            var dados = repo.SelecionarTodos();
+            var dados = repo.SelecionarTodos().Value;
 
             Assert.AreEqual(2, dados.Count);
 
@@ -55,48 +51,42 @@ namespace LocadoraVeiculos.Testes.TestesIntegradorBanco.TesteIntegradoGrupoVeicu
         public void DeveVerificarExistenciaGrupoVeiculos()
         {
             ServicoGrupoVeiculos repo = new ServicoGrupoVeiculos();
-
             GrupoVeiculos gveh = new GrupoVeiculos("Grupo 1");
-
             repo.InserirNovo(gveh);
 
             var exite = repo.Existe(gveh._id);
 
-            Assert.IsTrue(exite);
+            Assert.IsTrue(exite.Value);
         }
 
         [TestMethod]
         public void DeveEditarGrupoVeiculos()
         {
             ServicoGrupoVeiculos repo = new ServicoGrupoVeiculos();
-
             GrupoVeiculos gveh = new GrupoVeiculos("Grupo 2");
-
             repo.InserirNovo(gveh);
 
             GrupoVeiculos gveh2 = new GrupoVeiculos("Grupo 3");
             gveh2._id = gveh._id;
             repo.Editar(gveh2);
 
-            var gveiculosBanco = repo.SelecionarPorId(gveh2._id);
+            var gvehNovo = repo.SelecionarPorId(gveh2._id).Value;
 
-            Assert.AreEqual(gveh2, gveiculosBanco);
+            Assert.AreEqual(gvehNovo, gveh2);
         }
 
         [TestMethod]
         public void DeveDeletarGrupoVeiculos()
         {
             ServicoGrupoVeiculos repo = new ServicoGrupoVeiculos();
-
             GrupoVeiculos gveh = new GrupoVeiculos("Grupo 2");
-
             repo.InserirNovo(gveh);
 
-            repo.Excluir(gveh._id);
+            repo.Excluir(repo.SelecionarPorId(gveh._id).Value);
 
             var existe = repo.Existe(gveh._id);
 
-            Assert.IsFalse(existe);
+            Assert.IsFalse(existe.Value);
         }
     }
 }
