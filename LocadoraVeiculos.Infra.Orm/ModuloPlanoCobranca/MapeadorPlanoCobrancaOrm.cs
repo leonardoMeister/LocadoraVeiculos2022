8e15e97f84460b40f72bbda01245c5d2ecfd8387
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-07-26 15:24:52 -0300

    ORM Plano Cobranca

---
+++ LocadoraVeiculos.Infra.Orm/ModuloPlanoCobranca/MapeadorPlanoCobrancaOrm.cs
@@ -0,0 +1,27 @@
+﻿using LocadoraVeiculos.Dominio.ModuloPlanoCobranca;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Infra.Orm.ModuloPlanoCobranca
+{
+    public class MapeadorPlanoCobrancaOrm : IEntityTypeConfiguration<PlanoCobranca>
+    {
+        public void Configure(EntityTypeBuilder<PlanoCobranca> builder)
+        {
+            builder.ToTable("TBPlanoCobranca");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.TipoPlano).IsRequired();
+            builder.Property(x => x.ValorDia).IsRequired();
+            builder.Property(x => x.LimiteKM).IsRequired();
+            builder.Property(x => x.ValorKM).IsRequired();
+
+            builder.HasOne(x => x.GrupoVeiculos)
+                .WithMany();
+        }
+    }
+}
