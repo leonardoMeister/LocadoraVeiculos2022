1900a53b37ddf28a0870d4048e97c0ade0e2ca5e
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-07-27 14:29:27 -0300

    Infra Orm Devolução e Locação

---
+++ LocadoraVeiculos.Infra.Orm/ModuloDevolucao/MapeadorDevolucaoOrm.cs
@@ -0,0 +1,16 @@
+﻿using LocadoraVeiculos.Dominio.ModuloDevolucao;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraVeiculos.Infra.Orm.ModuloDevolucao
+{
+    public class MapeadorDevolucaoOrm : IEntityTypeConfiguration<Devolucao>
+    {
+        public void Configure(EntityTypeBuilder<Devolucao> builder)
+        {
+            builder.ToTable("TB_DEVOLUCAO");
+            builder.Property(x => x.Id).ValueGeneratedNever().IsUnicode();
+            builder.Property(x => x.Nome).HasColumnType("varchar(100)").IsRequired();
+        }
+    }
+}
