c2e3f8988301aa8d9004581d9eb68746fa1b2390
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-07-29 16:25:17 -0300

    Validador Locação

--- LocadoraVeiculos.Dominio/ModuloLocacao/ValidadorLocacao.cs
+++ LocadoraVeiculos.Dominio/ModuloLocacao/ValidadorLocacao.cs
@@ -10,7 +10,23 @@ namespace LocadoraVeiculos.Dominio.ModuloLocacao
         readonly Regex regEx = new Regex("^[a-zA-Z0-9- ]*$");
         public ValidadorLocacao()
         {
-           
+            RuleFor(x => x.Veiculo)
+                .NotNull().WithMessage("Veiculo deve ser selecionado.");
+
+            RuleFor(x => x.Condutores)
+                .NotNull().WithMessage("Condutores deve ser selecionado.");
+
+            RuleFor(x => x.Cliente)
+                .NotNull().WithMessage("Cliente deve ser selecionado.");
+
+            RuleFor(x => x.GrupoVeiculos)
+                .NotNull().WithMessage("Grupo de Veiculos deve ser selecionado.");
+
+            RuleFor(x => x.PlanoCobranca)
+                .NotNull().WithMessage("Plano Cobrança deve ser selecionado.");
+
+            RuleFor(x => x.ListaTaxas)
+                .NotNull().WithMessage("Lista de Taxas não pode ser Null.");
         }
     }
 }
