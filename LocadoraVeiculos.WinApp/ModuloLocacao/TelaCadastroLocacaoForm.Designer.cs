d3cf6268ba8e91290f7d3dcda205027d3f9949fa
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-07-29 16:21:50 -0300

    att de correcao de erros da tela de cadastro de locacao

--- LocadoraVeiculos.WinApp/ModuloLocacao/TelaCadastroLocacaoForm.Designer.cs
+++ LocadoraVeiculos.WinApp/ModuloLocacao/TelaCadastroLocacaoForm.Designer.cs
@@ -45,11 +45,15 @@
             this.btnSalvar = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.dataDevolucao = new System.Windows.Forms.DateTimePicker();
-            this.dataLocacao = new System.Windows.Forms.DateTimePicker();
-            this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.listaTaxas = new System.Windows.Forms.CheckedListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbNivelTanque = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtKmInicial = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -58,7 +62,7 @@
             // 
             this.cmbCliente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbCliente.FormattingEnabled = true;
-            this.cmbCliente.Location = new System.Drawing.Point(387, 8);
+            this.cmbCliente.Location = new System.Drawing.Point(387, 47);
             this.cmbCliente.Name = "cmbCliente";
             this.cmbCliente.Size = new System.Drawing.Size(170, 28);
             this.cmbCliente.TabIndex = 0;
@@ -66,7 +70,7 @@
             // textoCLiente
             // 
             this.textoCLiente.AutoSize = true;
-            this.textoCLiente.Location = new System.Drawing.Point(326, 11);
+            this.textoCLiente.Location = new System.Drawing.Point(326, 50);
             this.textoCLiente.Name = "textoCLiente";
             this.textoCLiente.Size = new System.Drawing.Size(55, 20);
             this.textoCLiente.TabIndex = 32;
@@ -75,7 +79,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 11);
+            this.label1.Location = new System.Drawing.Point(66, 50);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 20);
             this.label1.TabIndex = 34;
@@ -85,7 +89,7 @@
             // 
             this.cmbCondutor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbCondutor.FormattingEnabled = true;
-            this.cmbCondutor.Location = new System.Drawing.Point(143, 8);
+            this.cmbCondutor.Location = new System.Drawing.Point(143, 47);
             this.cmbCondutor.Name = "cmbCondutor";
             this.cmbCondutor.Size = new System.Drawing.Size(175, 28);
             this.cmbCondutor.TabIndex = 33;
@@ -112,10 +116,10 @@
             // pictureBoxFoto
             // 
             this.pictureBoxFoto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxFoto.Location = new System.Drawing.Point(117, 81);
+            this.pictureBoxFoto.Location = new System.Drawing.Point(117, 131);
             this.pictureBoxFoto.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBoxFoto.Name = "pictureBoxFoto";
-            this.pictureBoxFoto.Size = new System.Drawing.Size(368, 203);
+            this.pictureBoxFoto.Size = new System.Drawing.Size(368, 153);
             this.pictureBoxFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxFoto.TabIndex = 38;
             this.pictureBoxFoto.TabStop = false;
@@ -123,7 +127,7 @@
             // labelFoto
             // 
             this.labelFoto.AutoSize = true;
-            this.labelFoto.Location = new System.Drawing.Point(69, 81);
+            this.labelFoto.Location = new System.Drawing.Point(69, 103);
             this.labelFoto.Name = "labelFoto";
             this.labelFoto.Size = new System.Drawing.Size(42, 20);
             this.labelFoto.TabIndex = 37;
@@ -131,11 +135,13 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.txtKmInicial);
+            this.groupBox1.Controls.Add(this.label10);
             this.groupBox1.Controls.Add(this.pictureBoxFoto);
             this.groupBox1.Controls.Add(this.cmbVeiculo);
             this.groupBox1.Controls.Add(this.labelFoto);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(26, 127);
+            this.groupBox1.Location = new System.Drawing.Point(26, 166);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(531, 299);
             this.groupBox1.TabIndex = 39;
@@ -145,7 +151,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 48);
+            this.label3.Location = new System.Drawing.Point(11, 87);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(126, 20);
             this.label3.TabIndex = 41;
@@ -154,7 +160,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 445);
+            this.label4.Location = new System.Drawing.Point(24, 484);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(113, 20);
             this.label4.TabIndex = 44;
@@ -164,7 +170,7 @@
             // 
             this.cmbPlanoCobranca.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbPlanoCobranca.FormattingEnabled = true;
-            this.cmbPlanoCobranca.Location = new System.Drawing.Point(145, 442);
+            this.cmbPlanoCobranca.Location = new System.Drawing.Point(145, 481);
             this.cmbPlanoCobranca.Name = "cmbPlanoCobranca";
             this.cmbPlanoCobranca.Size = new System.Drawing.Size(412, 28);
             this.cmbPlanoCobranca.TabIndex = 43;
@@ -173,7 +179,7 @@
             // 
             this.cmbGrupoVeiculo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbGrupoVeiculo.FormattingEnabled = true;
-            this.cmbGrupoVeiculo.Location = new System.Drawing.Point(143, 45);
+            this.cmbGrupoVeiculo.Location = new System.Drawing.Point(143, 84);
             this.cmbGrupoVeiculo.Name = "cmbGrupoVeiculo";
             this.cmbGrupoVeiculo.Size = new System.Drawing.Size(414, 28);
             this.cmbGrupoVeiculo.TabIndex = 45;
@@ -181,7 +187,7 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(306, 672);
+            this.btnCancelar.Location = new System.Drawing.Point(306, 711);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(123, 52);
             this.btnCancelar.TabIndex = 50;
@@ -191,7 +197,7 @@
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(436, 672);
+            this.btnSalvar.Location = new System.Drawing.Point(436, 711);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(123, 52);
             this.btnSalvar.TabIndex = 49;
@@ -202,7 +208,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(27, 486);
+            this.label5.Location = new System.Drawing.Point(27, 525);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(124, 20);
             this.label5.TabIndex = 52;
@@ -211,41 +217,24 @@
             // dataDevolucao
             // 
             this.dataDevolucao.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dataDevolucao.Location = new System.Drawing.Point(387, 90);
+            this.dataDevolucao.Location = new System.Drawing.Point(436, 129);
             this.dataDevolucao.Name = "dataDevolucao";
-            this.dataDevolucao.Size = new System.Drawing.Size(170, 27);
+            this.dataDevolucao.Size = new System.Drawing.Size(121, 27);
             this.dataDevolucao.TabIndex = 53;
-            // 
-            // dataLocacao
-            // 
-            this.dataLocacao.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dataLocacao.Location = new System.Drawing.Point(143, 90);
-            this.dataLocacao.Name = "dataLocacao";
-            this.dataLocacao.Size = new System.Drawing.Size(130, 27);
-            this.dataLocacao.TabIndex = 54;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(37, 92);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(100, 20);
-            this.label6.TabIndex = 55;
-            this.label6.Text = "Data Locação";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(281, 95);
+            this.label7.Location = new System.Drawing.Point(314, 132);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 20);
+            this.label7.Size = new System.Drawing.Size(116, 20);
             this.label7.TabIndex = 56;
-            this.label7.Text = "Data Locação";
+            this.label7.Text = "Data Devolução";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(281, 631);
+            this.label8.Location = new System.Drawing.Point(281, 670);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(153, 20);
             this.label8.TabIndex = 57;
@@ -254,21 +243,74 @@
             // listaTaxas
             // 
             this.listaTaxas.FormattingEnabled = true;
-            this.listaTaxas.Location = new System.Drawing.Point(37, 509);
+            this.listaTaxas.Location = new System.Drawing.Point(37, 548);
             this.listaTaxas.Name = "listaTaxas";
             this.listaTaxas.Size = new System.Drawing.Size(522, 114);
             this.listaTaxas.TabIndex = 58;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(56, 12);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 20);
+            this.label9.TabIndex = 59;
+            this.label9.Text = "Locação Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(143, 9);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(414, 27);
+            this.txtId.TabIndex = 60;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 20);
+            this.label6.TabIndex = 62;
+            this.label6.Text = "Nível Tanque";
+            // 
+            // cmbNivelTanque
+            // 
+            this.cmbNivelTanque.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNivelTanque.FormattingEnabled = true;
+            this.cmbNivelTanque.Location = new System.Drawing.Point(143, 131);
+            this.cmbNivelTanque.Name = "cmbNivelTanque";
+            this.cmbNivelTanque.Size = new System.Drawing.Size(165, 28);
+            this.cmbNivelTanque.TabIndex = 61;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(54, 72);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(133, 20);
+            this.label10.TabIndex = 39;
+            this.label10.Text = "Km Inicial Locação";
+            // 
+            // txtKmInicial
+            // 
+            this.txtKmInicial.Location = new System.Drawing.Point(282, 69);
+            this.txtKmInicial.Name = "txtKmInicial";
+            this.txtKmInicial.Size = new System.Drawing.Size(203, 27);
+            this.txtKmInicial.TabIndex = 40;
             // 
             // TelaCadastroLocacaoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 733);
+            this.ClientSize = new System.Drawing.Size(569, 792);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbNivelTanque);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.listaTaxas);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.dataLocacao);
             this.Controls.Add(this.dataDevolucao);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.btnCancelar);
@@ -316,10 +358,14 @@
         private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.DateTimePicker dataDevolucao;
-        private System.Windows.Forms.DateTimePicker dataLocacao;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.CheckedListBox listaTaxas;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbNivelTanque;
+        private System.Windows.Forms.MaskedTextBox txtKmInicial;
+        private System.Windows.Forms.Label label10;
     }
 }
